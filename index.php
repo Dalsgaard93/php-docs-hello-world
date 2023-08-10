cd22e567034b889b395c88211b6175ffc2220528
Alex <alda@netcompany.com>
2023-08-10 17:29:55 +0200

    sidste

--- index.php
+++ index.php
@@ -18,8 +18,7 @@
     </h4>
       
     <?php
-        echo "hi";
-/*
+
         if (isset($_GET['code'])) {
             // If code is present, display that
             echo "This is your to get token pair:";
@@ -28,16 +27,16 @@
             echo '<br />';
 
         } else {
-            */
+   
             //Generate connect-link
-        $client_id="aiiapoc-92cd7c26-3ca6-404d-9b1c-3dee11a15c81";
-        $redirect_uri="https://aiia-test-site.azurewebsites.net/";
-        $connect_link="https://api-sandbox.aiia.eu/v1/oauth/connect?client_id=".$client_id."&redirect_uri=".$redirect_uri."&response_type=code";
-        
-        echo "Please follow this link to give consent to aiia: <br />";
-        echo $connect_link;
+            $client_id="aiiapoc-92cd7c26-3ca6-404d-9b1c-3dee11a15c81";
+            $redirect_uri="https://aiia-test-site.azurewebsites.net/";
+            $connect_link="https://api-sandbox.aiia.eu/v1/oauth/connect?client_id=".$client_id."&redirect_uri=".$redirect_uri."&response_type=code";
+            
+            echo "Please follow this link to give consent to aiia: <br />";
+            echo $connect_link;
             //"<a href=$connect_link title='Page to go to' class='whatEver'>Anchor text</a>";
-       // }
+        }
     ?>
   
    <!-- <form method="post">
