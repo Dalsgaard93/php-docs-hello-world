b4e38783ec7555490e598844e6e0cab86d22fdc5
Dalsgaard93 <47177264+Dalsgaard93@users.noreply.github.com>
2023-08-10 15:00:57 +0200

    jajaja

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 echo "Så virker det kraftedme - bøh - hallllloooooo kom frem for helvede ";
 
+$url_components = parse_url( $url, $component = -1 )
+
+parse_str($url_components['query'], $params);
+     
+// Display result
+echo ' Hi '.$params['code'];
+
 ?>
