da9fe00f4dd64e681e1c992f4d792796dc15e56c
Dalsgaard93 <47177264+Dalsgaard93@users.noreply.github.com>
2023-08-10 15:08:10 +0200

    dfsgfds

--- index.php
+++ index.php
@@ -2,11 +2,28 @@
 
 echo "Så virker det kraftedme - bøh - hallllloooooo kom frem for helvede ";
 
+/*
+//if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
+    $url = "https://";   
+//else  
+    $url = "http://";   
+// Append the host(domain name, ip) to the URL.   
+$url.= $_SERVER['HTTP_HOST'];   
+
+// Append the requested resource location to the URL   
+$url.= $_SERVER['REQUEST_URI'];    
+
+echo $url;  
+
+
+//$url = 
+
 $url_components = parse_url( $url, $component = -1 )
 
 parse_str($url_components['query'], $params);
-     
+*/
+
 // Display result
-echo ' Hi '.$params['code'];
+echo ' Hi '.$_GET['code'];
 
 ?>
