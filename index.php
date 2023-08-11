e5186a8bf9840aaac4033f058da5a7efebe7e8d3
Alex <alda@netcompany.com>
2023-08-11 10:28:58 +0200

    fgh

--- index.php
+++ index.php
@@ -30,17 +30,15 @@
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, "https://api-sandbox.aiia.eu/v1/oauth/token");
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                //'Authorization: Basic Y29ubmVjdGVkX2NsaWVudHMtMjdkNGMyZjQtNmVmMy00OGE0LTkzZDYtZTA3OGVmYzcwNWQ4OjhmNTg4NDhmYWMzYTE5NTJkZjE2ZWY3MDY2ODVlYTkxZDJmNDlhYjM4MmMwZjJlMjljMjg0MmYxN2Q3OGQxZGU=',
                 'X-Client-Id: aiiapoc-92cd7c26-3ca6-404d-9b1c-3dee11a15c81',
                 'X-Client-Secret: 6e6c150ebcb36f90e8cd5c750c8c0ca42a8751b7d63f0110a465115dff4dec86',
-                'Cache-Control: no-cache',
                 'Content-Type: application/json'
             ]);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(["grant_type" => "authorization_code", "redirect_uri" => "https://aiia-test-site.azurewebsites.net/", "code" => utf8_decode($_GET['code'])]));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
             $code_exchange = json_decode(curl_exec($ch));
             curl_close($ch);
-            echo $code_exchange
+            echo $code_exchange;
 
 
 
